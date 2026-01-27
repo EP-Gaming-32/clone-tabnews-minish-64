4283e3b73dd33fd327ee1dc5efe4834ab1cb5743
deb13 <eap.dev2004@gmail.com>
2026-01-26 21:18:10 -0300

    Mensagem de WIP + Readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # clone-tabnews-minish-64
 
 Repositório para aulas do curso.dev
+
+Requistos: npm
+
+Setup inicial:
+
+- Instalar as dependências via:
+  `npm install`
+- Iniciar o servidor via `npm run dev`
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <>
-      <h1>Fortnite! ඞ</h1>
+      <h1>Nada para ver aqui! volte mais tarde |°_°|</h1>
       <img src="https://i.imgur.com/pnOtOuO.jpeg" />
     </>
   );
